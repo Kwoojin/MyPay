c4ff4db70ddd87b644ad299dd7db4882d016f904
Kwoojin <dnwls488@naver.com>
2023-07-29 23:06:00 +0900

    common 추가

---
+++ membership-service/src/main/java/common/SelfValidating.java
@@ -0,0 +1,27 @@
+package common;
+
+import javax.validation.*;
+import java.util.Set;
+
+public abstract class SelfValidating<T> {
+
+    private Validator validator;
+
+    @SuppressWarnings("resource")
+    public SelfValidating() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    /**
+     * Evaluates all Bean Validations on the attributes of this
+     * instance.
+     */
+    @SuppressWarnings("unchecked")
+    protected void validateSelf() {
+        Set<ConstraintViolation<T>> violations = validator.validate((T) this);
+        if (!violations.isEmpty()) {
+            throw new ConstraintViolationException(violations);
+        }
+    }
+}
