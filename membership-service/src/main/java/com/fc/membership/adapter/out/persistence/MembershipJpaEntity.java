fd9f89a6dfcb703979c97a0e90eefa6ca5a944a7
Kwoojin <dnwls488@naver.com>
2023-08-21 21:56:51 +0900

    table name 지정 및 yaml 옵션 docker-compose.yaml 환경변수화

--- membership-service/src/main/java/com/fc/membership/adapter/out/persistence/MembershipJpaEntity.java
+++ membership-service/src/main/java/com/fc/membership/adapter/out/persistence/MembershipJpaEntity.java
@@ -5,15 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Objects;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "membership")
 @Entity
 public class MembershipJpaEntity {
 
