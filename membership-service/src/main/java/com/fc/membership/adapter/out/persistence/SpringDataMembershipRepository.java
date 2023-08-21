58ff38bce3c18bdf402e585bf3264a1c75cda65f
Kwoojin <dnwls488@naver.com>
2023-08-21 21:54:37 +0900

    이름 오타 변경
    SpringDateMembershipRepository -> SpringDataMembershipRepository

--- membership-service/src/main/java/com/fc/membership/adapter/out/persistence/SpringDataMembershipRepository.java
+++ membership-service/src/main/java/com/fc/membership/adapter/out/persistence/SpringDataMembershipRepository.java
@@ -2,5 +2,5 @@ package com.fc.membership.adapter.out.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SpringDateMembershipRepository extends JpaRepository<MembershipJpaEntity, Long> {
+public interface SpringDataMembershipRepository extends JpaRepository<MembershipJpaEntity, Long> {
 }
