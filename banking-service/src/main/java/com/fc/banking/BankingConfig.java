edcf0f32006f9ade0b7792c1034c9f5e2eff827d
Kwoojin <dnwls488@naver.com>
2023-08-27 21:37:45 +0900

    카프카 로깅 테스트

---
+++ banking-service/src/main/java/com/fc/banking/BankingConfig.java
@@ -0,0 +1,9 @@
+package com.fc.banking;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.fc.common")
+public class BankingConfig {
+}
---
+++ membership-service/src/main/java/com/fc/membership/MembershipConfig.java
@@ -0,0 +1,9 @@
+package com.fc.membership;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.fc.common")
+public class MembershipConfig {
+}
